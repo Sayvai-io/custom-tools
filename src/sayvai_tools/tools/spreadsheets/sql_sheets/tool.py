508f5a4b1730c96d5e0d3170e28749fced95979a
kedar <kedareshwar72381@gmail.com>
2023-10-20 15:22:22 +0530

    modified sql sheets

--- src/sayvai_tools/tools/spreadsheets/sql_sheets/tool.py
+++ src/sayvai_tools/tools/spreadsheets/sql_sheets/tool.py
@@ -3,6 +3,12 @@ from sqlalchemy import create_engine, text
 
 
 class SQLSheet:
+    name = "sqlsheet"
+    description = (
+        "Useful for exporting SQL query results to a spreadsheet."
+        "You can use this to generate reports."
+    )
+
     def __init__(self, uri: str, sheet_path: str = 'output.xlsx'):
         self.connection = create_engine(uri).connect()
         self.sheet_path = sheet_path
