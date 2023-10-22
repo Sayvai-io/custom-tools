7ceba9f7657a028989efc432d3949e8e93f0f6ef
kedar <kedareshwar72381@gmail.com>
2023-10-22 14:59:43 +0530

    added gsheets

--- src/sayvai_tools/tools/spreadsheets/sql_sheets/tool.py
+++ src/sayvai_tools/tools/spreadsheets/sql_sheets/tool.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
 
+from sayvai_tools.utils.sheets import GSheets
+
 
 class SQLSheet:
     name = "sqlsheet"
@@ -17,3 +19,24 @@ class SQLSheet:
         df = pd.read_sql_query(text(query), self.connection)
         df.to_excel(self.sheet_path, index=False)
         return f'Data has been exported to {self.sheet_path}'
+
+class SQLGSheet:
+    name = "sqlgsheet"
+    description = (
+        "Useful for exporting SQL query results to a G-Sheets."
+        "You can use this to generate reports."
+    )
+
+    def __init__(self, uri: str):
+        self.gs = GSheets()
+
+    def gsheets(self, query: str):
+        df = pd.read_sql_query(text(query), self.connection)
+        data_dict = df.to_dict('split')
+        data = data_dict['data'][0]
+        columns = data_dict['columns']
+        result = [columns]  # Start with the header
+        result.append(data)  # Add the data
+        self.gs.create_sheet()
+        self.gs.update_values(result)
+        return 'Data has been exported to Google Sheets'
