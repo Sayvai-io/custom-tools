bd53f6c7f45b894ef0348bd340c32c00becca2c9
Sanjaypranav <sanjaypriya195@gmail.com>
2023-10-31 21:34:55 +0530

    updated google forms

--- src/sayvai_tools/utils/__init__.py
+++ src/sayvai_tools/utils/__init__.py
@@ -3,6 +3,7 @@ from src.sayvai_tools.utils.database.dbchain import SQLDatabaseChain
 from src.sayvai_tools.utils.google.gcalendar import GCalendar
 from src.sayvai_tools.utils.google.mail import EmailSender
 from src.sayvai_tools.utils.google.sheets import GSheets
+from src.sayvai_tools.utils.google.forms import GForms
 from src.sayvai_tools.utils.voice.stt import STT
 from src.sayvai_tools.utils.voice.tts import ElevenlabsAudioStreaming
 
@@ -14,4 +15,5 @@ __all__ = [
     "EmailSender",
     "GSheets",
     "STT",
+    "GForms",
 ]
--- src/sayvai_tools/utils/google/forms.py
+++ src/sayvai_tools/utils/google/forms.py
@@ -8,57 +8,97 @@ from oauth2client import client, file, tools
 SCOPES = "https://www.googleapis.com/auth/forms.body"
 DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
 
-store = file.Storage('token.json')
-creds = None
-if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
-    creds = tools.run_flow(flow, store)
 
-form_service = discovery.build('forms', 'v1', http=creds.authorize(
-    Http()), discoveryServiceUrl=DISCOVERY_DOC, static_discovery=False)
+class GForms:
+    """Google Forms"""
+    def __init__(self):
+        store = file.Storage('token.json')
+        creds = None
+        if not creds or creds.invalid:
+            flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
+            creds = tools.run_flow(flow, store)
 
-# Request body for creating a form
-NEW_FORM = {
-    "info": {
-        "title": "Quickstart form",
-    }
-}
-
-# Request body to add a multiple-choice question
-NEW_QUESTION = {
-    "requests": [{
-        "createItem": {
-            "item": {
-                "title": "In what year did the United States land a mission on the moon?",
-                "questionItem": {
-                    "question": {
-                        "required": True,
-                        "choiceQuestion": {
-                            "type": "RADIO",
-                            "options": [
-                                {"value": "1965"},
-                                {"value": "1967"},
-                                {"value": "1969"},
-                                {"value": "1971"}
-                            ],
-                            "shuffle": True
-                        }
-                    }
-                },
-            },
-            "location": {
-                "index": 0
+        self.form_service = discovery.build('forms', 'v1', http=creds.authorize(
+            Http()), discoveryServiceUrl=DISCOVERY_DOC, static_discovery=False)
+        pass
+    
+    def create_form(self, title: str):
+        NEW_FORM = {
+            "info": {
+                "title": title,
             }
         }
-    }]
-}
+        result = self.form_service.forms().create(body=NEW_FORM).execute()
+        return result["formId"]
+    
+        # NEW_FORM = {
+        # "info": {
+        #     "title": "Quickstart form",
+        # }
+        #  }
 
-# Creates the initial form
-result = form_service.forms().create(body=NEW_FORM).execute()
+        # # Request body to add a multiple-choice question
+        # NEW_QUESTION = {
+        #     "requests": [{
+        #         "createItem": {
+        #             "item": {
+        #                 "title": "In what year did the United States land a mission on the moon?",
+        #                 "questionItem": {
+        #                     "question": {
+        #                         "required": True,
+        #                         "choiceQuestion": {
+        #                             "type": "RADIO",
+        #                             "options": [
+        #                                 {"value": "1965"},
+        #                                 {"value": "1967"},
+        #                                 {"value": "1969"},
+        #                                 {"value": "1971"}
+        #                             ],
+        #                             "shuffle": True
+        #                         }
+        #                     }
+        #                 },
+        #             },
+        #             "location": {
+        #                 "index": 0
+        #             }
+        #         }
+        #     }]
+        # }
+    def add_question(self, form_title: str, question: str, options: list) -> str:
+        # Creates the initial form
+        # Adds the question to the form
+        NEW_QUESTION = {
+            "requests": [{
+                "createItem": {
+                    "item": {
+                        "title": question,
+                        "questionItem": {
+                            "question": {
+                                "required": True,
+                                "choiceQuestion": {
+                                    "type": "RADIO",
+                                    "options": [
+                                        {"value": option} for option in options
+                                    ],
+                                    "shuffle": True
+                                }
+                            }
+                        },
+                    },
+                    "location": {
+                        "index": 0
+                    }
+                }
+            }]
+        }
+        question_setting = self.form_service.forms().batchUpdate(formId=self.create_form(title=form_title), body=NEW_QUESTION).execute()
+        return "Question has been added"
 
-# Adds the question to the form
-question_setting = form_service.forms().batchUpdate(formId=result["formId"], body=NEW_QUESTION).execute()
-
-# Prints the result to show the question has been added
-get_result = form_service.forms().get(formId=result["formId"]).execute()
-print(get_result)
+    def get_form(self, form_title: str):
+        # Prints the result to show the question has been added
+        # get form id from title 
+        form_id = self.form_service.forms().get(formId=form_title).execute()["formId"]
+        get_result = self.form_service.forms().get(formId=form_id).execute()
+        print(get_result)
+        return get_result
