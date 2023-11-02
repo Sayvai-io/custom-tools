d62c76b234b412e46a3df54e2db4f2a3e68944ac
kedar <kedareshwar72381@gmail.com>
2023-11-02 13:31:05 +0530

    updated tool.py

--- src/sayvai_tools/tools/send_mail/tool.py
+++ src/sayvai_tools/tools/send_mail/tool.py
@@ -1,18 +1,18 @@
 # Author: Aathikabilan
-import os
 import asyncio
-from sayvai_tools.utils import EmailSender
-from sayvai_tools.utils import Excel
+import os
 
-SMTP_USERNAME = os.getenv("SMTP_USERNAME")
-SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
-ORGANIZER_EMAIL = os.getenv("ORGANIZER_EMAIL")
+from sayvai_tools.utils import EmailSender, Excel
+
 
 class SendMail:
     name = "send_mail"
     description = "You can ask send_mail tool to send mail for you."
 
     def __init__(self, path: str):
+        SMTP_USERNAME = os.getenv("SMTP_USERNAME")
+        SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+        ORGANIZER_EMAIL = os.getenv("ORGANIZER_EMAIL")
         self.excel = Excel(path=path)
         self.emails = self.excel.read_column("email")
         self.email_sender = EmailSender(ORGANIZER_EMAIL, SMTP_USERNAME, SMTP_PASSWORD)
