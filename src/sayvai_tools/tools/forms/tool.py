513378e1c9e20621c3f7aa8e106e9db77e93e83f
Sanjaypranav <sanjaypriya195@gmail.com>
2023-11-03 09:06:28 +0530

    bug fixes

--- src/sayvai_tools/tools/forms/tool.py
+++ src/sayvai_tools/tools/forms/tool.py
@@ -4,14 +4,14 @@ from sayvai_tools.utils.google.forms import GForms
 
 
 class FormTool:
-    def __init__(self, scope: str, title: str = "Test Form""):
+    def __init__(self, scope: str, title: str = "Test Form"):
         self.scope = scope
         self.form = GForms(formTitle=title)
 
     name = "Form Tool"
     description = "Tool for forms"
 
-    def _run(self, questions: List[str]t):
+    def _run(self, questions: List[str]):
         self.form.add_multiple_questions(questions=questions)
         return "Questions have been added"
 
