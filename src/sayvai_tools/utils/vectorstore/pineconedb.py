8d8687ca0053a74c858b30eb4817318a251cb63a
sanjaypranav <sanjaypriya195@gmail.com>
2024-02-07 22:24:06 +0530

    updated langchain_pinecone

---
+++ src/sayvai_tools/utils/vectorstore/pineconedb.py
@@ -0,0 +1,16 @@
+import os
+from typing import Any
+from langchain_pinecone import Pinecone
+from langchain_openai import OpenAIEmbeddings
+
+
+class PineconeDB:
+    def __init__(self, embeddings: OpenAIEmbeddings | Any, index_name: str, **kwargs):
+        self.pinecone = Pinecone(
+            pinecone_api_key=kwargs.get("pinecone_api_key", os.environ.get("PINECONE_API_KEY")),
+            index_name=index_name,
+            embedding = embeddings
+        )
+
+    def _run(self, query: str, k: int = 3):
+        return self.pinecone.similarity_search(query=query, k=k)[1:]
