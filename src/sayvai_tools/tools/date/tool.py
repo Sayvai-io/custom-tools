551bdb5f7cae7c8f11f1a0961a599c807492c94f
Sanjaypranav <sanjaypriya195@gmail.com>
2024-02-12 13:49:18 +0530

    return type to string

--- src/sayvai_tools/tools/date/tool.py
+++ src/sayvai_tools/tools/date/tool.py
@@ -12,7 +12,7 @@ class GetDate:
     def _run(self, tool_input: str | None = None) -> str:
         """Use the tool."""
         current_time = datetime.datetime.now()
-        return current_time
+        return str(current_time)
 
     async def _arun(self) -> str:
         """Use the tool asynchronously."""
