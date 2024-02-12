d91aa31c0556a298e3fb8e04b447709447aeb9e7
Arun <prakaasharun50@gmail.com>
2024-02-12 11:26:54 +0530

    templates for google_calendar

--- templates/calendar-events/gcalendar/calendaragent.py
+++ templates/calendar-events/gcalendar/calendaragent.py
@@ -14,7 +14,13 @@ from sayvai_tools.tools.google_calendar import (AvailableSlotsTool,
 llm = ChatOpenAI(model="gpt-4")
 
 _SYSTEM_PROMPT: str = (
-    """ 
+    """
+    You have access to two tools: get_date and create_event. 
+    - get_date provides information about the current date and time.
+    - create_event allows you to create a new event in Google Calendar, with inputs for the start time, end time, and description of the event. The start and end times can be obtained from the get_date tool.
+
+    Use the information from the get_date tool to provide up-to-date responses and the create_event tool to schedule events accurately in Google Calendar. 
+    Additionally, you should be able to create events in Google Calendar using the create_event tool based on the user's specified time interval and description.
     """
 )
 
@@ -42,15 +48,26 @@ class DateTimeAgent:
         if self.tools is None:
             self.tools = [
                 Tool(
-                    name="AvailableSlots",
-                    func=AvailableSlotsTool(credentials=self.creds).run,
-                    description="Use this tool to find available time slots in a specified Google Calendar ( start time , end time , duration ) "
+                    name="create_event",
+                    func=CreateEventTool(credentials=self.creds).run,
+                    description="""
+                    A tool for creating events with Google Calendar. 
+                    It requires the following input parameters in the form of a dictionary:
 
+                    tool_input= {
+                        'summary': [A brief description of the event.]
+                        'start_time': [The start time of the event on a specific date.]
+                        'end_time': [The end time of the event on a specific date.]
+                    }
+
+                    Please provide these parameters in the 'tool_input' dictionary.
+                    """
                 ),
                 Tool(
                     func=GetDate()._run,
-                    name="GetDateTool",
-                    description="""A tool that returns the current date and time.""",
+                    name="get_date",
+                    description="""A tool that retrieves the current date and time. 
+                   This tool does not require any input parameters.""",
                 ),
             ]
         return "Tools Initialized"
@@ -75,4 +92,4 @@ class DateTimeAgent:
 dateagent = DateTimeAgent()
 dateagent.initialize_tools()
 dateagent.initialize_agent_executor()
-print(dateagent.invoke("what is  date after 4 days?"))
+print(dateagent.invoke("shedule an appointment tommorrow at 9.30pm for an hour for standup meeting"))
